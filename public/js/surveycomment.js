4c7a9a50a0f04a6e32ed5b7987e4d86ff6f08bb8
BrandonKYH <15brandon.jaramillo@gmail.com>
2024-04-05 23:59:55 +0800

    getting back lost changes

--- public/js/surveycomment.js
+++ public/js/surveycomment.js
@@ -1,20 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const commentInput = document.querySelector('.comment-input');
-    const btnAddComment = document.querySelector('.btn-add-comment');
-    const commentsSection = document.querySelector('.comments-section');
-    
-    btnAddComment.addEventListener('click', function () {
-      const commentText = commentInput.value;
-      if (commentText.trim() !== '') {
-        const newComment = document.createElement('div');
-        newComment.classList.add('comment');
-        
-        const timestamp = new Date().toLocaleString();
-        newComment.innerHTML = `<p><strong>You</strong> | ${timestamp}</p><p>${commentText}</p>`;
-        
-        commentsSection.appendChild(newComment);
-
-        commentInput.value = '';
-      }
-    });
-  });
+function toggleComments() {
+  const div = document.getElementById('commentsContainer');
+  if (div.style.display === 'none') {
+      div.style.display = 'block';
+  } else {
+      div.style.display = 'none';
+  }
+}
